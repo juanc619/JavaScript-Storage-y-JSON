3fca1ff5b2e9649d0b37fccd7fca675493427516
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-06-20 17:29:44 -0500

    guardando informacion localStorage

---
+++ main.js
@@ -0,0 +1,8 @@
+// localStorage
+// sessionStorage
+
+localStorage.setItem("iva", 21);
+localStorage.setItem("nombre", "juan carlos");
+
+let usiario = "jc";
+localStorage.setItem("usuario", usuario);
